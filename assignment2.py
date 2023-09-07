bf488bba37fc384ab76f16e1ba87285461151a9e
Lyn Quintana <68966779+lynqme@users.noreply.github.com>
2023-09-07 13:24:00 -0400

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,74 @@
+import math
+from socket import *
+
+
+class Assignment2:
+
+    def __init__(self, year=0):
+        self.year = year
+
+    def tellAge(self, currentYear):
+        global age
+        age = currentYear-self.year
+        print('Your age is ', age)
+
+    def listAnniversaries(self):
+        y = math.floor(age/10)
+        anniversaries=[]
+        count = 1
+        for x in range(y):
+            annis=count*10
+            anniversaries.append(annis)
+            count+=1
+        return anniversaries
+
+    def modifyYear(self, n):
+        result = ""
+        yearstr = str(self.year)
+        for x in range(n):
+            result += yearstr[0]
+            result += yearstr[1]
+        conv = str(self.year*n)
+        count=1
+        for x in range(len(conv)):
+            if(count%2>0):
+                result+=conv[count-1]
+            count+=1
+        return result
+
+
+    @staticmethod
+    def checkGoodString(string):
+       count = 0
+       amt = 0
+       for x in range(len(string)):
+           if (string[count].isnumeric()):
+               amt += 1
+           count += 1
+       if (len(string) >= 9) and (string[0].islower())  and (amt<=1):
+           return True
+       else:
+           return False
+
+    @staticmethod
+    def connectTcp(host, port):
+        try:
+            x = socket(AF_INET, SOCK_STREAM)
+            address = gethostbyname(host)
+            x.connect((address, port))
+            x.close()
+            return True
+        except:
+            return False
+
+obj = Assignment2(1782)
+obj.tellAge(2022)
+print(obj.listAnniversaries())
+print(obj.modifyYear(3))
+print(obj.checkGoodString("f1obar0more"))
+print(obj.checkGoodString("foobar0more"))
+retval = obj.connectTcp("www.google.com", 80)
+if retval:
+    print("Connection established correctly")
+else:
+    print("Some error")
